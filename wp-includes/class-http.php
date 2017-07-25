f5859cf1afd4ccf43eb2cd246e234e7fbd67c287
Julian Finkler <julian@developer-heaven.de>
2017-07-25 23:47:15 +0200

    requests implemented as composer dependency

--- wp-includes/class-http.php
+++ wp-includes/class-http.php
@@ -7,13 +7,6 @@
  * @since 2.7.0
  */
 
-if (!class_exists('Requests')) {
-    require(ABSPATH . WPINC . '/class-requests.php');
-
-    Requests::register_autoloader();
-    Requests::set_certificate_path(ABSPATH . WPINC . '/certificates/ca-bundle.crt');
-}
-
 /**
  * Core class used for managing HTTP transports and making HTTP requests.
  *
