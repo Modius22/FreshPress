29d45e71922c4dd9b5b6c2fe237d2063fcf1a7fd
Julian Finkler <julian@developer-heaven.de>
2017-08-18 23:34:25 +0200

    Fallback for WP_REST_Posts_Controller added

--- src/Core/WP2FPWrapper.php
+++ src/Core/WP2FPWrapper.php
@@ -93,6 +93,7 @@ $classes = array_change_key_case([
     'WP_REST_Comments_Controller' => 'Devtronic\\FreshPress\\Components\\Rest\\Endpoints\\CommentsController',
     'WP_REST_Post_Statuses_Controller' => 'Devtronic\\FreshPress\\Components\\Rest\\Endpoints\\PostStatusController',
     'WP_REST_Post_Types_Controller' => 'Devtronic\\FreshPress\\Components\\Rest\\Endpoints\\PostTypesController',
+    'WP_REST_Posts_Controller' => 'Devtronic\\FreshPress\\Components\\Rest\\Endpoints\\PostsController',
 ]);
 
 $abstractClasses = array_change_key_case([
