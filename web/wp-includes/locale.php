ad54505415e2f7a3c4cfc5e51a60dad9e347f8e3
Julian Finkler <julian@developer-heaven.de>
2017-09-01 20:58:30 +0200

    locale.php marked for removing

--- web/wp-includes/locale.php
+++ web/wp-includes/locale.php
@@ -1,10 +1,2 @@
 <?php
-/**
- * Locale API
- *
- * @package WordPress
- * @subpackage i18n
- * @since 1.2.0
- */
-
-_deprecated_file(basename(__FILE__), '4.7.0');
+// Only for compatibility, remove in 2.x
